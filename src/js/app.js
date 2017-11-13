676320cb1e61692b992130f86dae330fb515d398
Helga Helen <oliatrubina@yandex.ru>
2017-11-13 17:54:52 +0300

    testimonials slide

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
-  
   'use strict'
+
+  $('#fullpage').fullpage({
+    scrollHorizontally: false
+    , fixedElements: 'header'
+    , onLeave: function (index, nextIndex) {
+        if (nextIndex == 1) {
+          $('.burger_menu').removeClass('other');
+        } else {
+          $('.burger_menu').addClass('other');
+        }
+    }
+  });
   
+  $('.anchor').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveSectionDown();
+  });
+  
+  //testimonials slider
+  
+  $('.testimonials_slider').slick({
+    dots: true
+    , slidesToShow: 3
+    , slidesToScroll: 3
+    , autoplay: true
+    , autoplaySpeed: 8000
+    , variableWidth: true
+  });
   
 })
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,7 @@
 //= lib/head.js
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= lib/jquery.easings.min.js
+//= lib/jquery.fullpage.min.js
+//= lib/slick.min.js
 
 //= app.js
