e7bc127574ec61dd4fc6aaa3676fdd49ea805e4e
Helga Helen <oliatrubina@yandex.ru>
2017-11-15 15:37:47 +0300

    modals

--- src/js/app.js
+++ src/js/app.js
@@ -20,12 +20,42 @@ $(document).ready(function () {
     $.fn.fullpage.moveSectionDown();
   });
   
+  $('.anchor_application').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(6);
+  });
+  
+  $('.to_advantages').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(2);
+  });
+  
+  $('.to_testimonials').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(3);
+  });
+  
+  $('.to_steps').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(4);
+  });
+  
+  $('.to_team').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(5);
+  });
+  
+  $('.to_form').on('click', function (e) {
+    e.preventDefault();
+    $.fn.fullpage.moveTo(6);
+  });
+  
   //testimonials slider
   
   $('.testimonials_slider').slick({
     dots: true
-    , slidesToShow: 3
-    , slidesToScroll: 3
+    , slidesToShow: 2
+    , slidesToScroll: 1
     , autoplay: true
     , autoplaySpeed: 8000
     , variableWidth: true
@@ -45,5 +75,75 @@ $(document).ready(function () {
       },
     },
   });
+  
+  // select
+  
+  $('.js-select').select2({
+    minimumResultsForSearch: Infinity
+  });
+  
+  // popup
+  
+  $('.burger_menu').click(function (e) {
+        e.preventDefault();
+        $('.popup_nav').fadeIn(300);
+        $('body').css('overflow', 'hidden');   
+    });
+//    $('.popup_nav a').click(function (e) {
+//        e = event || window.event
+//        if (e.target == this) {
+//            $(this).fadeOut(100);
+//            $('body').css('overflow', 'auto'); 
+//        }
+//    });
+	$('.popup_close, .popup_nav a').click(function (e) {
+         e.preventDefault();
+           $('.popup_nav').fadeOut(100);
+           $('body').css('overflow', 'auto'); 
+    });
+//    $('.popup_content').click(function(e) {
+//		e.stopPropagation();
+//	});
+  
+  $('.js-popup').click(function (e) {
+        e.preventDefault();
+        var DataId = $(this).attr('data-id');
+        if (typeof DataId == "string") {
+        	$('#'+DataId).fadeIn(300);
+        	$('body').css('overflow', 'hidden');   
+        }
+    });
+    $('.popup').click(function (e) {
+        e = event || window.event
+        if (e.target == this) {
+            $(this).fadeOut(100);
+            $('body').css('overflow', 'auto'); 
+        }
+    });
+	$('.popup_close').click(function (e) {
+         e.preventDefault();
+           $(this).parents('.popup').fadeOut(100);
+           $('body').css('overflow', 'auto'); 
+    });
+    $('.popup_content').click(function(e) {
+		e.stopPropagation();
+	});
+  
+  $('.question_btn').on('click', function (e) {
+    $('.popup .subtitle').text('Задать вопрос');
+    $('.popup .btn').text('Задать вопрос');
+  });
+  $('.application_btn').on('click', function (e) {
+    $('.popup .subtitle').text('Оставить заявку');
+    $('.popup .btn').text('Оставить заявку');
+  });
+  
+  
+  
+  // custom scroll
+  
+  $('.popup.overlay.text .popup_content .content').perfectScrollbar();
+  
+  
   
 })
--- src/js/common.js
+++ src/js/common.js
@@ -4,6 +4,8 @@
 //= lib/jquery.fullpage.min.js
 //= lib/slick.min.js
 //= lib/swiper.min.js
-
+//= lib/select2.min.js
+//= lib/perfect-scrollbar.jquery.min.js
+//= lib/perfect-scrollbar.min.js
 
 //= app.js
